7d3e92decc1f9193b3132cb39705b72ebc08866b
yashwanthg7 <yashwanth18183@gmail.com>
2023-01-16 16:26:45 +0530

    Update counter

--- js/counter.js
+++ js/counter.js
@@ -6,7 +6,7 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-    if(count === null){
+    if(count == null){
         count =1;
     }
     else{
