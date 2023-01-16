782c4e22d6330865340b1da4adc2ce16e4483a7f
yashwanthg7 <yashwanth18183@gmail.com>
2023-01-16 16:23:26 +0530

    counter updated

--- js/counter.js
+++ js/counter.js
@@ -6,6 +6,12 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
+    if(count === null){
+        count =1;
+    }
+    else{
+        count = parseInt(count +1);
+    }
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
