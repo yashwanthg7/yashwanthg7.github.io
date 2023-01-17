9d6ded72cb26b444c199c4c35045e4c530f2e513
yashwanthg7 <yashwanth18183@gmail.com>
2023-01-17 14:17:57 +0530

    counter file updated

--- js/visitcount.js
+++ js/visitcount.js
@@ -3,14 +3,14 @@ function updateCounter() {
     //increment the counter everytime user visits the page by storing in local storage
     let count = localStorage.getItem('visiterCount');
     /**
+     
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-    if(count == null){
-        count = 1 ;
-    }
-    else{
-        count = parseInt(count) + 1 ;
+    if (count == null) {
+        count = "1";
+    } else {
+        count = parseInt(count) + 1;
     }
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
